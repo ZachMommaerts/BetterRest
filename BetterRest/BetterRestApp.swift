fd58c9285214ab22ab7aedcb83f7671fb5cda1d8
ZachMommaerts <zmommaerts@gmail.com>
2023-09-11 20:20:11 -0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Zach Mommaerts on 9/11/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
